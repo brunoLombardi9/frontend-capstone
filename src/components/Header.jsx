a52b238a40b5a646c2be01836a340cf6da063525
Bruno Lombardi <juegos.malafar@gmail.com>
2024-03-24 16:57:18 -0300

    project continues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/css/Header.css";
 import { Link } from "react-router-dom";
+import { MdMenu } from "react-icons/md";
+import { MdOutlineClose } from "react-icons/md";
 
 const routes = [
   { name: "Home", link: "/" },
@@ -12,12 +14,37 @@ const routes = [
 ];
 
 const Header = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <header>
       <Link to="/">
         <img src={require("../assets/LogoColor.png")} alt="logo" id="navLogo" />
       </Link>
-      <nav>
+
+      <div id="mobileScreenBtns">
+        {!nav ? (
+          <MdMenu size={50} onClick={() => setNav(true)} />
+        ) : (
+          <MdOutlineClose size={50} onClick={() => setNav(false)} />
+        )}
+      </div>
+
+      {nav && (
+        <nav id="mobileScreenNav">
+          <ul className={`${nav ? "showNav" : "hideNav"}`}>
+            {routes.map((r) => (
+              <li key={r.name}>
+                <Link to={r.link} className={`navLinks`}>
+                  {r.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+
+      <nav id="wideScreenNav">
         <ul>
           {routes.map((r) => (
             <li key={r.name}>
